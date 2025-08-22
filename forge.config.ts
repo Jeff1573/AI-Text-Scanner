59aee14e0e55ce3bfe561b9998c493476df74c3f
jf <https://gitlab.hunantianxing.com/>
2025-08-22 16:07:09 +0800

    chore: 更新版本号至v1.0.15，并优化forge.config.ts文件中的路径引号格式

--- forge.config.ts
+++ forge.config.ts
@@ -1,33 +1,26 @@
-import type { ForgeConfig } from '@electron-forge/shared-types';
-import { MakerSquirrel } from '@electron-forge/maker-squirrel';
-import { MakerZIP } from '@electron-forge/maker-zip';
-import { MakerDeb } from '@electron-forge/maker-deb';
-import { MakerRpm } from '@electron-forge/maker-rpm';
-import { VitePlugin } from '@electron-forge/plugin-vite';
-import { FusesPlugin } from '@electron-forge/plugin-fuses';
-import { FuseV1Options, FuseVersion } from '@electron/fuses';
-import { join } from 'path'
+import type { ForgeConfig } from "@electron-forge/shared-types";
+import { MakerSquirrel } from "@electron-forge/maker-squirrel";
+import { MakerZIP } from "@electron-forge/maker-zip";
+import { MakerDeb } from "@electron-forge/maker-deb";
+import { MakerRpm } from "@electron-forge/maker-rpm";
+import { VitePlugin } from "@electron-forge/plugin-vite";
+import { FusesPlugin } from "@electron-forge/plugin-fuses";
+import { FuseV1Options, FuseVersion } from "@electron/fuses";
+import { join } from "path";
 
 const config: ForgeConfig = {
   packagerConfig: {
     asar: true,
-    icon: join(__dirname, 'main/static/icons8-camera-256'),
+    icon: join(__dirname, "main/static/icons8-camera-256"),
   },
   rebuildConfig: {},
-  makers: [new MakerSquirrel({
-    setupIcon: join(__dirname, 'main/static/icons8-camera-256.ico'),
-  }), new MakerZIP({}, ['darwin']), new MakerRpm({}), new MakerDeb({})],
-  publishers: [
-    {
-      name: '@electron-forge/publisher-github',
-      config: {
-        repository: {
-          owner: 'cat9L',
-          name: 'fast-ocr'
-        },
-        prerelease: true
-      }
-    }
+  makers: [
+    new MakerSquirrel({
+      setupIcon: join(__dirname, "main/static/icons8-camera-256.ico"),
+    }),
+    new MakerZIP({}, ["darwin"]),
+    new MakerRpm({}),
+    new MakerDeb({}),
   ],
   plugins: [
     new VitePlugin({
@@ -36,20 +29,20 @@ const config: ForgeConfig = {
       build: [
         {
           // `entry` is just an alias for `build.lib.entry` in the corresponding file of `config`.
-          entry: 'main/main.ts',
-          config: 'vite.main.config.ts',
-          target: 'main',
+          entry: "main/main.ts",
+          config: "vite.main.config.ts",
+          target: "main",
         },
         {
-          entry: 'main/preload.ts',
-          config: 'vite.preload.config.ts',
-          target: 'preload',
+          entry: "main/preload.ts",
+          config: "vite.preload.config.ts",
+          target: "preload",
         },
       ],
       renderer: [
         {
-          name: 'main_window',
-          config: 'vite.renderer.config.ts',
+          name: "main_window",
+          config: "vite.renderer.config.ts",
         },
       ],
     }),
